88e94be756e727d0c6aab6dcfd8cac7e19976987
Prateek Agarwal <prateek@Prateeks-MacBook-Pro.local>
2021-12-22 13:51:21 +0900

    Added string functions

--- src/string/string-demo.c
+++ src/string/string-demo.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -34,4 +35,32 @@ int main(int argc, char const *argv[]) {
   strcat(full_name, first_name);
   strcat(full_name, last_name);
   printf("After using strcat, full_name is %s\n", full_name);
+  free(full_name);
+
+  // strstr
+  char haystack[] = "35=1|15=ABC|49=34.54";
+  char original_haystack[strlen(haystack) + 1];
+
+  // Copy haystack in original_haystack as tokenization is destructive.
+  strcpy(original_haystack, haystack);
+  char haystack_delim[] = "|";
+  printf("Haystack: %s\n", haystack);
+
+  char needle[8];
+  printf("What do you want to search in haystack?: ");
+  scanf("%s", &needle);
+  char *tokenized_haystack = strtok(haystack, haystack_delim);
+
+  bool is_found = false;
+  while (tokenized_haystack) {
+    char *found = strstr(tokenized_haystack, needle);
+    if (found) {
+      is_found = true;
+      fprintf(stdout, "Found %s\n", found);
+    }
+    tokenized_haystack = strtok(NULL, haystack_delim);
+  }
+  if (!is_found) {
+    fprintf(stdout, "Could not find %s in %s\n", needle, original_haystack);
+  }
 }
